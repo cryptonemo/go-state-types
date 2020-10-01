93a412d8e9fa8a08a8989ef0d0e78c98ee13682b
Steven Allen <steven@stebalien.com>
2020-10-01 09:29:32 -0700

    fill in new network versions
    This patch adds a version 5 (pending, may change), and provides a "version
    count" for convenience.

--- network/version.go
+++ network/version.go
@@ -7,11 +7,16 @@ import "math"
 type Version uint
 
 const (
-	Version0 = Version(iota) // specs-actors v0.9.3
-	Version1                 // specs-actors v0.9.7
-	Version2                 // specs-actors v0.9.?
-	Version3                 // Coming soon
-	Version4                 // Who knows?
+	Version0 = Version(iota) // 00000000: genesis   (specs-actors v0.9.3)
+	Version1                 // 00041280: breeze    (specs-actors v0.9.7)
+	Version2                 // 00051000: smoke     (specs-actors v0.9.)
+	Version3                 // 00094000: ignition  (specs-actors v0.9)
+	Version4                 // 00128888: actors v2 (specs-actors v2.0.x (future))
+	Version5                 // 00148888: liftoff   (specs-actors v2.0.x (future))
 
+	// VersionCount is the number of versions defined.
+	VersionCount
+
+	// VersionMax is the maximum version number
 	VersionMax = Version(math.MaxUint32)
 )
